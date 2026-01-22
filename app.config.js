640c47e283ac71348f4137c2da88eb32301ea1f2
Oscar <oscarcr14711@gmail.com>
2026-01-22 01:04:55 -0600

    removed libraries and did dev build

---
+++ app.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+    name: 'FlyRide',
+    slug: 'flyride',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+        image: './assets/splash.png',
+        resizeMode: 'contain',
+        backgroundColor: '#ffffff'
+    },
+    ios: {
+        supportsTablet: true,
+        bundleIdentifier: 'com.oscarcr714.flyride',
+        buildNumber: '1',
+        newArchEnabled: false,
+        infoPlist: {
+            NSCameraUsageDescription: 'FlyRide needs access to your camera to scan luggage and documents.',
+            NSPhotoLibraryUsageDescription: 'FlyRide needs access to your photo library to import travel documents.',
+            NSMicrophoneUsageDescription: 'FlyRide may use your microphone for video features.',
+            NSLocationWhenInUseUsageDescription: 'FlyRide uses your location to provide airport recommendations.',
+            ITSAppUsesNonExemptEncryption: false
+        }
+    },
+    android: {
+        adaptiveIcon: {
+            foregroundImage: './assets/adaptive-icon.png',
+            backgroundColor: '#ffffff'
+        },
+        package: 'com.oscarcr714.flyride',
+        versionCode: 1,
+        permissions: [
+            'CAMERA',
+            'READ_EXTERNAL_STORAGE',
+            'WRITE_EXTERNAL_STORAGE',
+            'ACCESS_FINE_LOCATION',
+            'NOTIFICATIONS'
+        ]
+    },
+    web: {
+        favicon: './assets/favicon.png'
+    },
+    plugins: [
+
+        'expo-document-picker',
+        'expo-file-system',
+        'expo-image-picker',
+        'expo-notifications',
+        [
+            'expo-av',
+            {
+                microphonePermission: 'Allow FlyRide to access your microphone for video features.'
+            }
+        ]
+    ],
+    extra: {
+        eas: {
+            projectId: 'b2b41890-2d3c-405d-bb1e-9d0a40f6d2c5'
+        }
+    }
+};
